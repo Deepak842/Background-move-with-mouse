92ed35ff5c37c429706ccd028b510808029d0674
deepak sah <deepaksah>
2018-01-07 21:17:46 +0530

    first commit

---
+++ js/mouse.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+     var object1=$('#object1');
+         var object2=$('#object2');
+         var object3=$('#object3');
+            
+          var layer=$('#layer');
+         
+         layer.mousemove(function(e){
+             var valueX=(e.pageX * -1 / 12);
+             var valueY=(e.pageY * -1 / 12);
+             
+             object1.css({
+                 'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+             });
+         });
+         layer.mousemove(function(e){
+             var valueX=(e.pageX * -1 / 15);
+             var valueY=(e.pageY * -1 / 20);
+             
+             object2.css({
+                 'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+             });
+         });
+    layer.mousemove(function(e){
+             var valueX=(e.pageX * -1 / 15);
+             var valueY=(e.pageY * -1 / 15);
+             
+             object3.css({
+                 'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+             });
+         });
+    
+});
+    
